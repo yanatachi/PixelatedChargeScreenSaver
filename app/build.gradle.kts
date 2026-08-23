e1bc0eada1122099889296e77aa4a7d76c0a5bc5
yanatachi <wai@example.com>
2026-08-23 23:48:10 +0900

    Update Version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yanatachi.pixelatedcharge"
         minSdk = 33
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
